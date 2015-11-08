2cf7d749cf0d45d319d7f0d0b1d521bd40a97475
Catherine Jin <csj3sd@virginia.edu>
2015-11-08 11:34:24 -0500

    new cs1 directory

--- code/python/hw15.py
+++ code/python/hw15.py
@@ -1,19 +1,3 @@
-'''
-works
-def hs (a):
-    print (a)
-    l = l + 1
-    if (a != 1):
-        if ((a % 2) == 0):
-            a = a/2
-        else:
-            a = 3*a + 1
-        hs (a)
-    else:
-        print (None)
-
-hs (12)
-'''
 def hs (a):
     l = 1
     if (a == 1):
